c3d3e2725ea976d639c87a4a2a09379c953a07a9
vritant24 <vritoku@gmail.com>
2017-06-10 23:16:14 +0530

    made minesweeper component

---
+++ src/Components/Minesweeper.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import MineField from './MineField';
+
+export default class Minesweeper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numRow: 10,         //number of rows of the field
+      numCol: 10,         //number of columns of the field
+      numMine: 10,        //number of mines in the field
+      numFlag: 0,         //number of flags used by the player
+      numOpen: 0,         //number of opened cells
+      time: 0,            //the timer displayed
+      status: "playing"   // playing, clear, gameover
+    };
+  }
+  componentWillUpdate() {
+    if(this.state.status === "playing") {
+      this.judge();
+    }
+  }
+  componentWillMount() {
+    this.intervals = [];
+  }
+  tick() {
+    if(this.state.numOpen > 0 && this.state.status === "playing") {
+      this.setState({time: this.state.time + 1});
+    }
+  }
+  judge() {
+    if(this.state.numOpen + this.state.numMine >= this.state.numRow * this.state.numCol) {
+      this.setState({status: "clear"});
+    }
+  }
+  gameOver() {
+    this.setState({status: "gameover"});
+  }
+  checkNumFlag(update) {
+    this.setState({numFlag: this.state.numFlag + update});
+  }
+  addNumOpen() {
+    if(this.state.numOpen === 0) {
+      this.interval = setInterval(this.tick.bind(this), 1000);
+    }
+    this.setState({numOpen: this.state.numOpen + 1});
+  }
+  reset() {
+    clearInterval(this.interval);
+    this.setState({numOpen: 0, numFlag: 0, time: 0, status: "playing"});
+  }
+  render() {
+    return(
+      <div className="Minesweeper">
+        <span className="numFlag"> {this.state.numMine - this.state.numFlag} </span>
+        <span className="face" onClick={this.reset.bind(this)}>
+          <span className={"button " + this.state.status} />
+        </span>
+        <span className="time"> {this.state.time} </span>
+        <MineField numOpen={this.state.numOpen} numMine={this.state.numMine} numRow={this.state.numRow}
+                  numCol={this.state.numCol} gameOver={this.gameOver.bind(this)} addNumOpen={this.addNumOpen.bind(this)}
+                  checkNumFlag={this.checkNumFlag.bind(this)} />
+      </div>
+    );
+  }
+}
